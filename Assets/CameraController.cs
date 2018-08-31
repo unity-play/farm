203b889b3dd8a7261c3562a385ffc1b96237377e
Iulian Rotaru <iulian@gnotis.ro>
2018-08-31 21:45:36 +0300

    Initial import.

---
+++ Assets/CameraController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public float sensitivity = 4;
+
+    private Vector3 offset;
+    private bool firstPerson;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+        if (offset == Vector3.zero)
+        {
+            firstPerson = true;
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+
+        if(Input.GetMouseButton(0))
+        {
+            float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+            float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
+
+            Vector3 rotation = transform.rotation.eulerAngles;
+            rotation.x += mouseY;
+            rotation.y -= mouseX;
+
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+        else
+        {
+            if (firstPerson)
+            {
+                //transform.Rotate(Vector3.up * player.transform.rotation.y);
+                transform.rotation = Quaternion.Euler(0, player.transform.eulerAngles.y, 0);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0, player.transform.eulerAngles.y, 0);
+            }
+
+        }
+
+        //transform.LookAt(player.transform.position - player.transform.forward * 10f);
+
+        //transform.position = player.transform.position - player.transform.forward * 4f;
+        //transform.LookAt(player.transform);
+
+        //transform.rotation = player.transform.rotation;
+
+        //Vector3 rotate = Vector3.zero;
+        //rotate.y = player.transform.rotation.y;
+        //transform.Rotate(Vector3.up, rotate.y * Time.deltaTime);
+    }
+}
---
+++ Assets/ObjectMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMove : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float velocity = 4.0f;
+        if (Input.GetButton("Jump"))
+        {
+            velocity = 6 * velocity;
+        }
+
+        float x = velocity * Input.GetAxis("Horizontal");
+        float y = 0;
+        float z = velocity * Input.GetAxis("Vertical");
+        //transform.Translate(x * Time.deltaTime, y * Time.deltaTime, z * Time.deltaTime);
+        transform.Translate(0, 0, z * Time.deltaTime);
+
+
+    }
+}
